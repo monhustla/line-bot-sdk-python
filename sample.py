7743584b707984823a0bce18cbf14c84e6fd6e27
monhustla <rdavis23344@gmail.com>
2017-05-30 18:21:18 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -40,9 +40,11 @@ def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text="I'm taking this to the team."))
-    
-for userId in events:    
-line_bot_api.multicast([event.userId], TextSendMessage(text='Hello World!'))   
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.multicast(
+        event.userId,
+        TextSendMessage(text='Hello World!'))   
  
 
 
