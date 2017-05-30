77e9d7abc77ef5e613e55660c8007cfca1c46d60
monhustla <rdavis23344@gmail.com>
2017-05-30 17:05:29 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,75 +1,39 @@
-import os
-import sys
-import wsgiref.simple_server
-from linebot import LineBotApi
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
-from linebot.exceptions import LineBotApiError
-from argparse import ArgumentParser
-from linebot import (
-    LineBotApi, WebhookParser
-)
-from linebot.utils import PY3
-
-channel_secret = os.getenv('Line_Channel_Secret', None)
 channel_access_token = os.getenv('Line_Channel_Access_Token', None)
 if channel_secret is None:
     print('Specify LINE_CHANNEL_SECRET as environment variable.')
-    
+    sys.exit(1)
 if channel_access_token is None:
     print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
-    
+    sys.exit(1)
 
-line_bot_api=LineBotApi(channel_access_token)
-parser= WebhookParser(channel_secret)
+line_bot_api = LineBotApi(channel_access_token)
+handler = WebhookHandler(channel_secret)
 
-def application(environ, start_response):
-    # check request path
-    if environ['PATH_INFO'] != '/callback':
-        start_response('404 Not Found', [])
-        return create_body('Not Found')
 
-    # check request method
-    if environ['REQUEST_METHOD'] != 'POST':
-        start_response('405 Method Not Allowed', [])
-        return create_body('Method Not Allowed')
-
+@app.route("/callback", methods=['POST'])
+def callback():
     # get X-Line-Signature header value
-    signature = environ['HTTP_X_LINE_SIGNATURE']
+    signature = request.headers['X-Line-Signature']
 
     # get request body as text
-    wsgi_input = environ['wsgi.input']
-    content_length = int(environ['CONTENT_LENGTH'])
-    body = wsgi_input.read(content_length).decode('utf-8')
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
 
+    # handle webhook body
     try:
-        events=parser.parse(body,signature)
+        handler.handle(body, signature)
     except InvalidSignatureError:
-        start_response('400 Bad Request',[])
-        return create_body ('Bad Request')
-    for event in events:
-        if not isinstance(event, MessageEvent):
-            continue
-        if not isinstance(event.message, TextMessage):
-            continue
-    
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text='hey'))
+        abort(400)
 
-    start_response('200 OK', [])
-    return create_body('OK')
-
-def create_body(text):
-    if PY3:
-        return [bytes(text, 'utf-8')]
-    else:
-        return text
+    return 'OK'
 
 
-if __name__ == '__main__':
-    arg_parser = ArgumentParser(
-        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
-    )
-    arg_parser.add_argument('-p', '--port', default=8000, help='port')
-    options = arg_parser.parse_args()
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text="Hey"))
 
-    httpd = wsgiref.simple_server.make_server('', options.port, application)
-    httpd.serve_forever()
+
+if __name__ == "__main__":
+    app.run()
