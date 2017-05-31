8c3473dd1547e2a6592ca88fd01e55b7b4e6eee4
monhustla <rdavis23344@gmail.com>
2017-05-31 11:23:20 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -15,7 +15,7 @@ def callback():
     json_line = request.get_json()
     json_line = json.dumps(json_line)
     decoded = json.loads(json_line)
-    user = decoded["events"][0]['userId']
+    user = decoded["events"][0]['source']['userId']
     #id=[d['replyToken'] for d in user][0]
     #print(json_line)
     print("ok",user)
@@ -33,6 +33,9 @@ def sendText(user, text):
  
     data = json.dumps({
         "replyToken":user,
+        "source":{
+            "userId":userId
+        }
         "messages":[{
             "type":"text",
             "text":text
