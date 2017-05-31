4f9a39368eed27a6821bbcb656ded627ff608931
monhustla <rdavis23344@gmail.com>
2017-05-30 20:36:01 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,4 +1,3 @@
-
 from __future__ import unicode_literals
 
 import errno
@@ -81,11 +80,5 @@ def handle_text_message(event):
          TextSendMessage(text="hey")))
 
 if __name__ == "__main__":
-        arg_parser = ArgumentParser(
-            usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
-    )
-        arg_parser.add_argument('-p', '--port', default=8000, help='port')
-        arg_parser.add_argument('-d', '--debug', default=False, help='debug')
-        options = arg_parser.parse_args()
-        make_static_tmp_dir()
-        app.run(debug=options.debug, port=options.port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
