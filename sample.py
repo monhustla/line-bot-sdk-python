513557550acb75dd2ef83752e109b35671f656ba
monhustla <rdavis23344@gmail.com>
2017-05-31 15:41:26 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -34,52 +34,6 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('jCCJTBH9PKP0UzrCtVCpT99E2kOPn3bowhUA8KX1hcxMHwqdZbfLzP/I6leONvKqZmNyqKC1w/2pZYau7cKtSQePM/Wb+Vj8t3F9XbyRavOLgd/1Y6PUccEc5/8ce/BJjGcGlHH0T/7l2nUlpqsAIgdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('2f8bf1b9951192d713bc216bdc585df2')
 
-new=open("userids.txt","w")
-
-@app.route('/')
-def index():
-    return "Hello World!"
-
-@app.route('/callback', methods=['POST'])
-def callback():
-    json_line = request.get_json()
-    json_line = json.dumps(json_line)
-    decoded = json.loads(json_line)
-    user = decoded['events'][0]['source']['userId']
-    #id=[d['replyToken'] for d in user][0]
-    #print(json_line)
-    with open("userids.txt",'w') as outfile:
-        json.dump(user,outfile)
-    
-    print("ok",user)
-    print (user)
-    return '',200
-    
-@app.route
-
- 
-def sendText(user, text):
-    LINE_API = 'https://api.line.me/v2/bot/message/multicast'
-    Authorization = 'jCCJTBH9PKP0UzrCtVCpT99E2kOPn3bowhUA8KX1hcxMHwqdZbfLzP/I6leONvKqZmNyqKC1w/2pZYau7cKtSQePM/Wb+Vj8t3F9XbyRavOLgd/1Y6PUccEc5/8ce/BJjGcGlHH0T/7l2nUlpqsAIgdB04t89/1O/w1cDnyilFU='
- 
-    headers = {
-        'Content-Type': 'application/json; charset=UTF-8',
-        'Authorization':Authorization
-    }
-    data = user
-    new=open("userids.txt","w")
-    z=str(user)
-    new.write(z)
-    print(z)
-    new.close()
-    
- 
- 
-    r = requests.post(LINE_API, headers=headers, data=data) 
-    #print(r.text)
-
-
-
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+new=open('userids.txt','w')
+new.write('user')
+new.close()
