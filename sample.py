becda9f77e00914218b944c04f277b45662453ed
monhustla <rdavis23344@gmail.com>
2017-05-31 13:52:20 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -3,8 +3,35 @@ import json
 import requests
 import os
 import sys
+import errno
+import os
+import sys
+import tempfile
+from argparse import ArgumentParser
+
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+    SourceUser, SourceGroup, SourceRoom,
+    TemplateSendMessage, ConfirmTemplate, MessageTemplateAction,
+    ButtonsTemplate, URITemplateAction, PostbackTemplateAction,
+    CarouselTemplate, CarouselColumn, PostbackEvent,
+    StickerMessage, StickerSendMessage, LocationMessage, LocationSendMessage,
+    ImageMessage, VideoMessage, AudioMessage,
+    UnfollowEvent, FollowEvent, JoinEvent, LeaveEvent, BeaconEvent
+)
  
 app = Flask(__name__)
+
+line_bot_api = LineBotApi('jCCJTBH9PKP0UzrCtVCpT99E2kOPn3bowhUA8KX1hcxMHwqdZbfLzP/I6leONvKqZmNyqKC1w/2pZYau7cKtSQePM/Wb+Vj8t3F9XbyRavOLgd/1Y6PUccEc5/8ce/BJjGcGlHH0T/7l2nUlpqsAIgdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('2f8bf1b9951192d713bc216bdc585df2')
  
 @app.route('/')
 def index():
@@ -15,7 +42,7 @@ def callback():
     json_line = request.get_json()
     json_line = json.dumps(json_line)
     decoded = json.loads(json_line)
-    user = decoded['events'][0]['source']['userId']
+    user = decoded['events'][0]['source']
     #id=[d['replyToken'] for d in user][0]
     #print(json_line)
     print("ok",user)
@@ -33,10 +60,12 @@ def sendText(user, text):
     data = user
     
  
-    
+ 
     r = requests.post(LINE_API, headers=headers, data=data) 
     #print(r.text)
- 
+
+if user is !=0:
+    line_bot_api.push_message(user,TextSendMessage(text="You Did It"))
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
