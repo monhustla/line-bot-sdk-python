401473c791f569fe4eb220e06b19063a121b5e52
monhustla <rdavis23344@gmail.com>
2017-05-30 20:07:58 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -79,17 +79,14 @@ def handle_text_message(event):
     line_bot_api.push_message(
         (event.source.user_id,
          TextSendMessage(text="hey"))
-
+     
 
 if __name__ == "__main__":
-    arg_parser = ArgumentParser(
-        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
+        arg_parser = ArgumentParser(
+            usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
     )
-    arg_parser.add_argument('-p', '--port', default=8000, help='port')
-    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
-    options = arg_parser.parse_args()
-
-    # create tmp dir for download content
-    make_static_tmp_dir()
-
-    app.run(debug=options.debug, port=options.port)
+        arg_parser.add_argument('-p', '--port', default=8000, help='port')
+        arg_parser.add_argument('-d', '--debug', default=False, help='debug')
+        options = arg_parser.parse_args()
+        make_static_tmp_dir()
+        app.run(debug=options.debug, port=options.port)
