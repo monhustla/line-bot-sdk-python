27068caa7d2904d97adaae519b1f0c6e306e36cc
monhustla <rdavis23344@gmail.com>
2017-07-02 17:33:33 -0500

    Update sampletwo.py

--- sampletwo.py
+++ sampletwo.py
@@ -53,13 +53,17 @@ def callback():
             continue
         if not isinstance(event.message, TextMessage):
             continue
-        if event.message.text== "Maps":
-            continue
-
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=event.message.text)
-        )
+        if event.message.text is "Maps":
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text="Congratulations")
+            )
+            
+            else:
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    TextSendMessage(text=event.message.text)
+                )
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
