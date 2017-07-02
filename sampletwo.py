3985b628629eebf7388213c79f96900ff6e42754
monhustla <rdavis23344@gmail.com>
2017-07-02 17:53:12 -0500

    Update sampletwo.py

--- sampletwo.py
+++ sampletwo.py
@@ -57,19 +57,10 @@ def callback():
             line_bot_api.reply_message(
                 event.reply_token,
                 TextSendMessage(text="Congratulations")
-            )
             
-        else:
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text=event.message.text)
-            )
+       
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text="Hey"))
+
 
 
 if __name__ == "__main__":
