b14b304454ad8e4a108c2d1d5bec134e9e1de947
monhustla <rdavis23344@gmail.com>
2017-07-02 14:15:41 -0500

    Update sampletwo.py

--- sampletwo.py
+++ sampletwo.py
@@ -47,11 +47,4 @@ def handle_message(event):
 
 
 if __name__ == "__main__":
-    arg_parser = ArgumentParser(
-        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
-    )
-    arg_parser.add_argument('-p', '--port', default=8000, help='port')
-    options = arg_parser.parse_args()
-
-    httpd = wsgiref.simple_server.make_server('', options.port, application)
-    httpd.serve_forever()
+    app.run(environ.get('PORT'))
