90bae9dd3460eea9e8521fadfabf939d94809f60
monhustla <rdavis23344@gmail.com>
2017-07-02 20:04:46 -0500

    Update sampletwo.py

--- sampletwo.py
+++ sampletwo.py
@@ -63,7 +63,9 @@ def callback():
             line_bot_api.reply_message(
                 event.reply_token,
                 ImageSendMessage(
-                    original_content_url='http://i.imgur.com/Y9XKDz9.jpg')
+                    original_content_url='http://i.imgur.com/Y9XKDz9.jpg',
+                    preview_image_url='https://example.com/preview.jpg'
+                )
             )
             
        
