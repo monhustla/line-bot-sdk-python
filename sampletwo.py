ad1f2218a30e0e05c286ee269618557b4bfc1cfc
monhustla <rdavis23344@gmail.com>
2017-07-02 21:51:20 -0500

    Update sampletwo.py

--- sampletwo.py
+++ sampletwo.py
@@ -38,6 +38,11 @@ handler = WebhookHandler('2f8bf1b9951192d713bc216bdc585df2')
 line_bot_api = LineBotApi('jCCJTBH9PKP0UzrCtVCpT99E2kOPn3bowhUA8KX1hcxMHwqdZbfLzP/I6leONvKqZmNyqKC1w/2pZYau7cKtSQePM/Wb+Vj8t3F9XbyRavOLgd/1Y6PUccEc5/8ce/BJjGcGlHH0T/7l2nUlpqsAIgdB04t89/1O/w1cDnyilFU=')
 parser = WebhookParser('2f8bf1b9951192d713bc216bdc585df2')
 
+@handler.add(JoinEvent)
+def handle_join(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text='Hey, thanks for the invite. For a list of my commands type "list" ' + event.source.type))
 
 @app.route("/callback", methods=['POST'])
 def callback():
