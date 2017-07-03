49b77321722931f1a95c1cdd20c0362b5dee19c7
monhustla <rdavis23344@gmail.com>
2017-07-02 23:15:54 -0500

    Update sampletwo.py

--- sampletwo.py
+++ sampletwo.py
@@ -114,22 +114,7 @@ def make_static_tmp_dir():
             raise
 
 
-@app.route("/callback", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
 
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    return 'OK'
 
 @handler.add(JoinEvent)
 def handle_join(event):
