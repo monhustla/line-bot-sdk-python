112f34e2b32e00062839851aaef742c0133b22cb
monhustla <rdavis23344@gmail.com>
2017-07-04 13:17:43 -0500

    Update sampletwo.py

--- sampletwo.py
+++ sampletwo.py
@@ -51,7 +51,7 @@ password = url.password
 host = url.hostname
 port = url.port
 
-con = psycopg2.connect(
+conn = psycopg2.connect(
             dbname=dbname,
             user=user,
             password=password,
