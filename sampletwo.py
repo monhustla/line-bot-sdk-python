9c0f4ddccf604cdf2f8c1a573b44d661132ffdd1
monhustla <rdavis23344@gmail.com>
2017-07-03 23:35:50 -0500

    Update sampletwo.py

--- sampletwo.py
+++ sampletwo.py
@@ -47,6 +47,7 @@ url=urlparse.urlparse(os.environ["DATABASE_URL"])
 conn=psycopg2.connect(
     database=url.path[1:],
     user=url.username,
+    password=url.password,
     host=url.hostname,
     port=url.port
 )
