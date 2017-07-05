6991930fe499873dfefee6cca9f8612210def41f
monhustla <rdavis23344@gmail.com>
2017-07-05 14:00:32 -0500

    Update sampletwo.py

--- sampletwo.py
+++ sampletwo.py
@@ -129,10 +129,11 @@ def callback():
             rows=cur.fetchall()
             for row in rows:
                 content=("   ", row[0],":", row[sig])
+                print (content)
                 data=str(content)
                 line_bot_api.reply_message(
                     event.reply_token,
-                    TextSendMessage(text=data))
+                    TextSendMessage(text=content))
             #profile= line_bot_api.get_profile(user)
             #name=(profile.display_name)
             #cur=conn.cursor()
