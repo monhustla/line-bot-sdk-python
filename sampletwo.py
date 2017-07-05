06c934aadcb5258c63c3c774c67dbc596e9cc5ae
monhustla <rdavis23344@gmail.com>
2017-07-05 18:36:47 -0500

    Update sampletwo.py

--- sampletwo.py
+++ sampletwo.py
@@ -139,10 +139,11 @@ def callback():
 
             cur.execute("""INSERT INTO prestige_data(lineid, summoner_name, champ1_name, champ1_prestige, champ2_name, champ2_prestige, champ3_name, champ3_prestige, champ4_name, champ4_prestige, champ5_name, champ5_prestige) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);""",
                         (f, name, champ, h, '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}'))
-            cur.execute("SELECT lineid, summoner_name,champ1_name, champ1_prestige, champ2_name, champ2_prestige, champ3_name, champ3_prestige, champ4_name, champ4_prestige, champ5_name, champ5_prestige FROM prestige_data""")
+            cur.execute("SELECT lineid, summoner_name,champ1_name, champ1_prestige, champ2_name, champ2_prestige, champ3_name, champ3_prestige, champ4_name, champ4_prestige, champ5_name, champ5_prestige FROM prestige_data  WHERE lineid= %(lineid)s""",
+                        {"lineid":f})
             rows=cur.fetchall()
             for row in rows:
-                print((row['summoner_name']+":" + row['champ1_prestige'] "\n"))
+                print(row['summoner_name']+ ":" + row['champ1_prestige'] "\n")
                 line_bot_api.reply_message(
                     event.reply_token,
                     TextSendMessage(text=(row['summoner_name']+":" + row['champ1_prestige'] "\n"))
