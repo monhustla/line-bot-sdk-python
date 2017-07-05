45fd905f43fe098461ede7abf93350024c42d7a8
monhustla <rdavis23344@gmail.com>
2017-07-05 14:32:51 -0500

    Update sampletwo.py

--- sampletwo.py
+++ sampletwo.py
@@ -131,13 +131,13 @@ def callback():
             cur=conn.cursor()
             cur.execute("SELECT * FROM prestige where stars_champ_rank=%(stars_champ_rank)s",{"stars_champ_rank":champ1})
             rows=cur.fetchall()
-            for row in rows:
-                content=("   ", row[sig1])
-                print (content)
-                data=str(content)
-                line_bot_api.reply_message(
-                    event.reply_token,
-                    TextSendMessage(text=content))
+            content=("   ", row[sig1])
+            print (content)
+            data=str(content)
+            print (data)
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=data))
             #profile= line_bot_api.get_profile(user)
             #name=(profile.display_name)
             #cur=conn.cursor()
