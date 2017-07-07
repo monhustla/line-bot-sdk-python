06a29c9dddc809ad3e2fc841f306af030259353e
monhustla <rdavis23344@gmail.com>
2017-07-07 13:28:24 -0500

    Update sampletwo.py

--- sampletwo.py
+++ sampletwo.py
@@ -90,7 +90,7 @@ def calculate_prestige(champs):
     print (top_champcount)
     top_champamount=sum(results)
     print (top_champamount)
-    average=str((top_champamount/top_champcount))
+    average=str(float(top_champamount/top_champcount))
     print (average)
     
     
