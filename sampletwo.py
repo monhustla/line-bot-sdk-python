23860b19bafb684e5df043548c59fe26251d319c
monhustla <rdavis23344@gmail.com>
2017-07-07 13:36:51 -0500

    Update sampletwo.py

--- sampletwo.py
+++ sampletwo.py
@@ -270,15 +270,17 @@ def callback():
             for row in rows:
                 yay=calculate_prestige(row[2])
                 print (rows)
+                print (yay)
                 msg = ("Youre prestige is:"+yay)
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    TextSendMessage(text=msg))
                                                             # we should only have one result, but we'll stop just in case
             # The user does not exist in the database already
-            else:
-                msg = "Oops! You need to add some champs first. Try 'mc3 input champ'."
+            #else:
+                #msg = "Oops! You need to add some champs first. Try 'mc3 input champ'."
 
-            line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=msg))
+
                 
                    
                         
