ef5e8ea62aef16896e2d9e70b7818ced1d36daff
monhustla <rdavis23344@gmail.com>
2017-07-06 23:06:00 -0500

    Update sampletwo.py

--- sampletwo.py
+++ sampletwo.py
@@ -67,7 +67,7 @@ conn = psycopg2.connect(
 
 def get_prestige_for_champion(champ, sig):
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    cur.execute("SELECT prestige FROM prestige_table WHERE champ ='%(champ)s' AND sig ='%(sig)s'""", {"champ":champ, "sig": sig})
+    cur.execute("SELECT prestige FROM prestige_table WHERE champ ='%(champ)s' AND sig ='%(sig)s'""", {"champ":champ, "sig": [sig]})
     rows = cur.fetchall()
     for row in rows:
         return str(row['prestige'])                               # Returns a prestige value
